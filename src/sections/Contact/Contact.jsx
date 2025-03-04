732cb6aadaa057abe90b4377e6a36f13bcc8eeaa
rudrakadel <kdrudra20047@gmail.com>
2025-03-04 23:29:16 +0530

    Form Live

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formspree.io/f/mldgqyzd" method="POST">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -22,7 +22,7 @@ function Contact() {
             Email
           </label>
           <input
-            type="text"
+            type="email"
             name="email"
             id="email"
             placeholder="Email"
